0e5ee6e3e306761e8687223a6ab05ff561f174f5
Ihor Horbunov <igorgorbunov3333@gmail.com>
2021-04-06 20:36:16 +0300

    add sequence, add liquibase

--- ingestion-service/src/main/java/com/company/ingestion/domain/entity/RegulatoryDocumentType.java
+++ ingestion-service/src/main/java/com/company/ingestion/domain/entity/RegulatoryDocumentType.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 @AllArgsConstructor
 public class RegulatoryDocumentType {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "regulatory_document_type_seq")
+    @SequenceGenerator(name = "regulatory_document_type_seq", sequenceName = "regulatory_document_type_seq", allocationSize = 1)
     private Long id;
 
     @Column(nullable = false)
--- ingestion-service/src/main/resources/db/changelog/changes/init.sql
+++ ingestion-service/src/main/resources/db/changelog/changes/init.sql
@@ -5,3 +5,5 @@ create table regulatory_document_type (
     id bigint primary key not null,
     type text not null
 );
+
+CREATE SEQUENCE IF NOT EXISTS regulatory_document_type_seq;
